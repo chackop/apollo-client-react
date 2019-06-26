9f2b81292f599a121dae961d89877d349e26a37e
Chacko <chackop@gmail.com>
2019-06-26 18:29:04 +0100

    subscribetomore

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import { getMainDefinition } from 'apollo-utilities'
 import { split } from 'apollo-link'
 
 import { ApolloProvider } from 'react-apollo'
-// import Cars from './Cars'
-// import CarsTwo from './CarsTwo'
-// import CarsQuery from './CarsQuery'
-// import CarsRefetch from './CarsRefetch'
-// import CarsMutation from './CarsMutation'
+import Cars from './Cars'
+import CarsTwo from './CarsTwo'
+import CarsQuery from './CarsQuery'
+import CarsRefetch from './CarsRefetch'
+import CarsMutation from './CarsMutation'
 import CarsMutationComponent from './CarsMutationComponent'
 import AllCars from './AllCars'
+import QuerySubscription from './QuerySubscription'
 
 const httpLink = new HttpLink({
   uri: `http://localhost:4000/graphql`
@@ -44,7 +45,7 @@ const App = () => {
   return (
     <ApolloProvider client={client}>
       <CarsMutationComponent />
-      <AllCars />
+      <QuerySubscription />
     </ApolloProvider>
   )
 }
